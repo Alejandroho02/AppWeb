34e2eafbfeda6825d8ccb27ab3b2204493bacaf9
Diego23242 <94395761+Diego23242@users.noreply.github.com>
2024-10-03 23:55:38 -0700

    aa
    aver

--- ejemploweb/resources/views/index.blade.php
+++ ejemploweb/resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inside Battle</title>
-    <link rel="stylesheet" href="{{ mix('css/styles.css') }}">
+    <link rel="stylesheet" href="ejemploweb\resources\css\styles.css">
     </head>
 
 <body>
